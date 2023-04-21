ea2cf9ace0e9d2720dbdf5c84c51894c3800aa86
vpsmith54 <vpsmith54@gmail.com>
2023-04-20 20:25:44 -0700

    Creation

---
+++ src/main/java/com/vpsmith/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.vpsmith.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("app.datasource")
+	public DataSourceProperties dataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties("app.datasource.configuration")
+	public HikariDataSource dataSource(DataSourceProperties properties) {
+		return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

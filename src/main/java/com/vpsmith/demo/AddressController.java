a2597ad930ad328040e271dca0b884fcfb4a0503
vpsmith54 <vpsmith54@gmail.com>
2023-05-01 11:28:40 -0700

    use hateous

--- src/main/java/com/vpsmith/demo/AddressController.java
+++ src/main/java/com/vpsmith/demo/AddressController.java
@@ -2,9 +2,13 @@ package com.vpsmith.demo;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +21,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import static com.vpsmith.AddressMapper.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 @RequestMapping("/address")
 public class AddressController {
@@ -27,26 +35,45 @@ public class AddressController {
         this.service = service;
     }
 
-    @GetMapping
-    public List<AddressDTO> findAll() {
-        return service.findAll();
+    @GetMapping(produces = { "application/hal+json" })
+    public CollectionModel<AddressDTO> findAll() {
+        List<Address> addresses = service.findAll();
+        List<AddressDTO> dtos = new ArrayList<AddressDTO>();
+        for (Address address : addresses) {
+            Link selfLink = linkTo(AddressController.class).slash(address.getId()).withSelfRel();
+            AddressDTO dto = toDTO(address);
+            dto.add(selfLink);
+            dtos.add(dto);
+        }
+        Link aLink = linkTo(methodOn(AddressController.class).findAll()).withSelfRel();
+        CollectionModel<AddressDTO> model = CollectionModel.of(dtos, aLink);
+        return model;
     }
 
-    @GetMapping("{id}")
-    public AddressDTO findById(@PathVariable Long id) {
-        Optional<AddressDTO> address = service.findById(id);
-        return address.orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
-                "Unable to find the car " + id));
+    @GetMapping(value = "{id}", produces = { "application/hal+json" })
+    public EntityModel<AddressDTO> findById(@PathVariable Long id) {
+        Optional<Address> address = service.findById(id);
+        if (!address.isPresent()) {
+            throw new ResponseStatusException(NOT_FOUND,
+                    "Unable to find the address " + id);
+        }
+        Link selfLink = linkTo(AddressController.class).slash(id).withSelfRel();
+        AddressDTO dto = toDTO(address.get());
+        return EntityModel.of(dto, selfLink);
     }
 
-    @PostMapping
-    public ResponseEntity<AddressDTO> insert(@RequestBody AddressDTO dto) {
+    @PostMapping(produces = { "application/hal+json" })
+    public EntityModel<AddressDTO> insert(@RequestBody AddressDTO dto) {
         return new ResponseEntity<>(service.insert(dto), HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "{id}")
-    public AddressDTO update(@PathVariable("id") Long id, @RequestBody AddressDTO dto) {
-        return service.update(id, dto);
+    @PutMapping(value = "{id}", produces = { "application/hal+json" })
+    public EntityModel<AddressDTO> update(@PathVariable("id") Long id, @RequestBody AddressDTO dto) {
+        Address address = service.update(id, toEntity(dto));
+        Link selfLink = linkTo(AddressController.class).slash(id).withSelfRel();
+        AddressDTO returnDto = toDTO(address);
+        returnDto.add(selfLink);
+        return EntityModel.of(returnDto);
     }
 
     @DeleteMapping(value = "{id}")
